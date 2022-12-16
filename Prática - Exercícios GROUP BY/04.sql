869840dd157ee4f315b4b9ea2bf1d8b565b76da4
Fabio Caprara <fats140701@gmail.com>
2022-12-16 16:09:02 -0400

    feat: group by exercises add

---
+++ Prática - Exercícios GROUP BY/04.sql
@@ -0,0 +1,7 @@
+SELECT MAX(salary) AS "maximumSalary", roles.name AS role 
+FROM jobs 
+JOIN roles 
+ON jobs."roleId" = roles.id 
+WHERE jobs.active = true 
+GROUP BY role 
+ORDER BY "maximumSalary";
